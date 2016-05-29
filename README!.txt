d68b556a8ffe51c0316295dcfef8b4328d97c871
nick <nicksugar@outlook.com>
2016-05-29 03:04:52 -0600

    initial commit

---
+++ README!.txt
@@ -0,0 +1,7 @@
+This a single html page using angular for data that shows a table with multiple person driving records.
+
+This is as far as I've gotten so far.
+
+I want to add text feilds to add persons and buttons to add and remove persons. 
+
+Aslo filters to sort by name or state or whatever.
---
+++ driversRecords/recordsCtrl.js
@@ -0,0 +1,42 @@
+myApp.controller('records', function ($scope) {
+	$scope.drivers = [
+	{
+		"name": "Hari",
+		"SSN": "848-12-8376",
+		"age": "45",
+		"driversLicenseNumber": "27-239-9387",
+		"PointsOnRecord": "12",
+		"state": "Colorado"
+	},
+
+	{
+		"name":	"Jon",
+		"SSN": "214-12-8376",
+		"age": "26",
+		"driversLicenseNumber": "03-539-9387",
+		"PointsOnRecord": "12",
+		"state": "Colorado"
+	},
+
+
+	{
+		"name":	"Rob",
+		"SSN": "789-32-8376",
+		"age": "33",
+		"driversLicenseNumber": "98-139-4387",
+		"PointsOnRecord": "12",
+		"state": "Colorado"
+	},
+
+
+	{
+		"name":	"Nick",
+		"SSN": "211-19-4576",
+		"age": "31",
+		"driversLicenseNumber": "78-339-9737",
+		"PointsOnRecord": "12",
+		"state": "Colorado"
+	}
+
+	];
+});
